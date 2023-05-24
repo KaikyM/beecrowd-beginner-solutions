f7642812e6a0f623bcbc019c7cf38941345c4515
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-23 21:46:11 -0300

    BEE-1005-Java14.java

---
+++ BEE-1005-Java14.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        Double A = input.nextDouble();
+        Double B = input.nextDouble();
+        Double Av = ((3.5 * A) + (7.5 * B)) / 11;
+        System.out.printf("Average = %.5f\n", Av);
+        input.close();
+    }
+}
