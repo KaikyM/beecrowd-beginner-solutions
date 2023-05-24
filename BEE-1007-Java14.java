190133ffb6f98cbd0c48877cfeba286a6f4c0dba
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-24 19:15:06 -0300

    BEE-1007-Java14.java

---
+++ BEE-1007-Java14.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner S = new Scanner(System.in);
+        int A = S.nextInt();
+        int B = S.nextInt();
+        int C = S.nextInt();
+        int D = S.nextInt();
+        int Dif = ((A * B) - (C * D));
+        System.out.println("DIFFERENCE = " + Dif);
+    }
+}
