3464b85af00f0f42b598b97112982a99e4fd7a6d
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-25 00:23:00 -0300

    Create BEE-1009-Java14.java

---
+++ BEE-1009-Java14.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        String Name = input.next();
+        Double S = input.nextDouble();
+        Double B = input.nextDouble();
+        Double T = S + (B * 0.15);
+        System.out.printf("TOTAL = R$ %.2f\n", T);
+        input.close();
+    }
+}
