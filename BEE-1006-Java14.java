d23d757807a07070500c6b0c9078cb17293f3707
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 14:14:12 -0300

    Update BEE-1006-Java14.java

--- BEE-1006-Java14.java
+++ BEE-1006-Java14.java
@@ -1,17 +1,19 @@
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
-        Scanner Sc = new Scanner(System.in);
-        Double A = Sc.nextDouble();
-        Double B = Sc.nextDouble();
-        Double C = Sc.nextDouble();
-        int WA = 2;
-        int WB = 3;
-        int WC = 5;
-        Double Av = ((WA * A) + (WB * B) + (WC * C))/(WA + WB + WC) ;
-        System.out.printf("MEDIA = %.1f\n", Av);
-        Sc.close();
+   public static void main(String[] args) throws IOException {
+     Scanner Sc = new Scanner(System.in);
+     //
+     Double A = Sc.nextDouble();
+     Double B = Sc.nextDouble();
+     Double C = Sc.nextDouble();
+     int WA = 2;
+     int WB = 3;
+     int WC = 5;
+     //   
+     Double Av = ((WA * A) + (WB * B) + (WC * C))/(WA + WB + WC) ;
+     System.out.printf("MEDIA = %.1f\n", Av);
+     //
+     Sc.close();
     }
 }
