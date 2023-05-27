e2abb18df79ed012522d12d97841845b641579e7
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 14:18:55 -0300

    BEE-1010-Java14.java

--- BEE-1010-Java14.java
+++ BEE-1010-Java14.java
@@ -1,8 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 //      
         int CodPro1 = input.nextInt();
