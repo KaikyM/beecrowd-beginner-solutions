140704ed6f14a0ee6a2db851594b971c3c1de272
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 03:07:44 -0300

    Create BEE-1011-Java14.java

---
+++ BEE-1011-Java14.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        Double Pi = 3.14159;
+        Double R = input.nextDouble();
+        Double V = ((4/3.0) * Pi * Math.pow(R, 3));
+        System.out.printf("VOLUME = %.3f\n", V);
+        input.close();
+    }
+}
