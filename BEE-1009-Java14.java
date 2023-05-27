9750066dab69c8a7c3a5b05b3e22940f72758387
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 14:17:55 -0300

    BEE-1009-Java14.java

--- BEE-1009-Java14.java
+++ BEE-1009-Java14.java
@@ -4,11 +4,14 @@ public class Main {
  
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
+        //
         String Name = input.next();
         Double S = input.nextDouble();
         Double B = input.nextDouble();
         Double T = S + (B * 0.15);
+        //
         System.out.printf("TOTAL = U$ %.2f\n", T);
+        //
         input.close();
     }
 }
