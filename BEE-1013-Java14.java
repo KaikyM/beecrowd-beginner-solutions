514e45fb218283d5c1192158ea25b31e1af6dbbc
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 17:28:38 -0300

    Update BEE-1013-Java14.java

--- BEE-1013-Java14.java
+++ BEE-1013-Java14.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner Sc = new Scanner(System.in);
