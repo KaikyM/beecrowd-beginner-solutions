cb6cf0a21c6953fd80299bf3e55c25242cca90d6
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 15:13:48 -0300

    BEE-1014-Java14.java

--- BEE-1014-Java14.java
+++ BEE-1014-Java14.java
@@ -1,1 +1,16 @@
-
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner S = new Scanner(System.in); 
+        //
+        int Km = S.nextInt();
+        Double L = S.nextDouble();
+        //
+        Double Km_L = Km / L;
+        //
+        System.out.printf("%.3f km/l\n", Km_L);
+        //
+        S.close();
+    }
+}
