21831bcadd125b44436a1b04457d38d6fb6f129b
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 16:17:33 -0300

    BEE-1015-Java14.java

--- BEE-1015-Java14.java
+++ BEE-1015-Java14.java
@@ -1,1 +1,18 @@
-
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        //
+        Double x1 = input.nextDouble();
+        Double y1 = input.nextDouble();
+        Double x2 = input.nextDouble();
+        Double y2 = input.nextDouble();
+        //
+        Double Distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        //
+        System.out.printf("%.4f\n", Distance);
+        //
+        input.close();
+    }
+}
