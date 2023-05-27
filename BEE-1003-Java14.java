34bd213959344cd10a9fc7a9e4d0d893b8be72b6
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-05-27 14:36:23 -0300

    Update BEE-1003-Java14.java

--- BEE-1003-Java14.java
+++ BEE-1003-Java14.java
@@ -1,13 +1,15 @@
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
- 
     public static void main(String[] args) throws IOException {
         Scanner sca = new Scanner(System.in);
+        //
         int A = sca.nextInt();
         int B = sca.nextInt();
         int Sum = A + B;
+        //
         System.out.println("SOMA = " + Sum);
+        //
         sca.close();
     }
 }
