3546d49a117a7c35ecdedcff1942f1f3b2821242
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-07-03 20:39:23 -0300

    Create BEE-1002-C99.c

---
+++ Beecrowd_Problems/BEE-1002-C99.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double Radius;
+    scanf("%lf", &Radius);
+    
+    double Pi = 3.14159;
+    double Area = Pi * pow(Radius, 2);
+    
+    printf("A=%.4f\n", Area);
+    return 0;
+}
