6a9bb821a6805b763fa371f34bffd0e9b053531e
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-07-09 19:46:07 -0300

    BEE-1037-Java14.java

--- Beecrowd_Problems/BEE-1037-Java14.java
+++ Beecrowd_Problems/BEE-1037-Java14.java
@@ -6,13 +6,13 @@ public class Main {
         //
         float N = Sc.nextFloat();
         //
-        if(0.0 <= N && N <= 25.0000){
+        if(0.00 <= N && N <= 25.00){
             System.out.println("Intervalo [0,25]");
-        }else if(25.00001 <= N && N <= 50.0000000){
+        }else if(25.01 <= N && N <= 50.00){
             System.out.println("Intervalo (25,50]");
-        }else if(50.0000001 <= N && N <= 75.000000000){
+        }else if(50.01 <= N && N <= 75.00){
             System.out.println("Intervalo (50,75]");
-        }else if(75.000000001 <= N && N <= 100.00000000000){
+        }else if(75.01 <= N && N <= 100.00){
             System.out.println("Intervalo (75,100]");
         }else{
             System.out.println("Fora de intervalo");
