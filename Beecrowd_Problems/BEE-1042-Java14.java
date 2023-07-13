113c808b7fc1931f9062751985b64055823060a4
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-07-12 23:40:29 -0300

    Create BEE-1042-Java14.java

---
+++ Beecrowd_Problems/BEE-1042-Java14.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        //
+        int A, B, C;
+        A = input.nextInt();
+        B = input.nextInt();
+        C = input.nextInt();
+        //
+        int[] Sort = new int[3];
+        Sort[0] = A;
+        Sort[1] = B;
+        Sort[2] = C;
+        Arrays.sort(Sort);
+        //
+        for(int i = 0; i < Sort.length; i++){
+            System.out.println(Sort[i]);
+        }
+        System.out.println("\n"+ A + "\n" + B + "\n" + C);
+        //
+        input.close();
+    }
+}
