6d2797b2ec92a1c70b36f4b2c68143a54b9b3a61
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-07-14 13:00:46 -0300

    Create BEE-1036-C99.c

---
+++ Beecrowd_Problems/BEE-1036-C99.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double A, B , C, Delta = 0, X1 = 0, X2 = 0;
+    
+    scanf("%lf %lf %lf", &A, &B, &C);
+    
+    Delta = sqrt(pow(B, 2) - (4 * A * C));
+    
+    if(Delta >= 0 && A != 0){
+        X1 = (-B + Delta) / (2 * A);
+        X2 = (-B - Delta) / (2 * A);
+        printf("R1 = %.5lf\n", X1);
+        printf("R2 = %.5lf\n", X2);
+    }else{
+        printf("Impossivel calcular\n");
+    }
+    
+    return 0;
+}
