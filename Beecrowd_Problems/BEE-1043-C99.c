3ffebb4ec0766d66687e1828f62d474bba069ea4
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-07-16 23:28:06 -0300

    Create BEE-1043-C99.c

---
+++ Beecrowd_Problems/BEE-1043-C99.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float A, B, C, triangle, trapezium;
+    
+    scanf("%f %f %f", &A, &B, &C);
+    
+    if((A < B + C) && (B < A + C) && (C < A + B)){
+        triangle = A + B + C;
+        printf("Perimetro = %.1lf\n", triangle);
+    }else{
+        trapezium = ((A + B) * C) / 2;
+        printf("Area = %.1lf\n", trapezium);
+    }
+    return 0;
+}
