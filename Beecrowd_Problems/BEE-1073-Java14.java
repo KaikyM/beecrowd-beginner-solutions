34cfd462b517b467e98fa165f28649e9a434991e
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-08-03 00:43:09 -0300

    Create BEE-1073-Java14.java

---
+++ Beecrowd_Problems/BEE-1073-Java14.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        //
+        int X = input.nextInt();
+        int stop = 0, Y = 2;
+        //
+        while(stop < X){
+            if(X % 2 == 0){
+                System.out.printf("%d^2 = %d\n",Y , Y*Y);
+                Y = Y + 2;
+                stop = stop + 2;
+            }else{ X--; }
+        }
+        //
+        input.close();
+    }
+}
