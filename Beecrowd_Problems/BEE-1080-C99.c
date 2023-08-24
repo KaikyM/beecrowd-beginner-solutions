32668ed5abea31d462973f6c24cfac9555c1275c
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-08-23 23:30:30 -0300

    Create BEE-1080-C99.c

---
+++ Beecrowd_Problems/BEE-1080-C99.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int High = 0, Pos = 1; 
+    //
+    for(int i = 1; i <= 100; i++){
+        int X;
+        scanf("%d",&X);
+        if(High < X){
+            High = X;
+            Pos = i;
+        }
+    }
+    printf("%d\n%d\n", High, Pos);
+    //
+    return 0;
+}
