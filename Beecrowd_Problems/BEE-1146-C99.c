1d89eeccf44212fdca1d3814739d79bbdd513184
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-08-23 23:49:57 -0300

    Create BEE-1146-C99.c

---
+++ Beecrowd_Problems/BEE-1146-C99.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    bool Stop = false;
+    /**/
+    while(Stop != true){
+        int N;
+        scanf("%d", &N);
+        if(N != 0){
+            int I = 1;
+            while(I < N){
+                printf("%d ", I);
+                I++;
+            }
+            printf("%d\n", I);
+        }else{
+            Stop = true;
+        }
+    }
+    return 0;
+}
